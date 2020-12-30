72edbe5ad21e441eb033e4432b6e28ac6544e616
vantage-ola <vantageola4@gmail.com>
2020-12-30 23:09:37 +0100

    Updated PORT

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
 from flask import Flask, render_template
+
+port = int(os.environ.get('PORT', 5000))
 
 app= Flask(__name__, template_folder='templates')
 
@@ -11,4 +14,4 @@ def tables():
     return render_template("tables.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
